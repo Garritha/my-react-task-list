e7202930f6ef163c398a15abbc08d49eee561bc7
Rafael Reales <rafitha.19960607@gmail.com>
2023-05-23 20:00:41 -0500

    primer commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,30 @@
+
+import styles from './header.module.css';
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+import { useState } from 'react';
+
+export function Header({ handleAddTask }) {
+  const [title, setTitle] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    handleAddTask(title);
+    setTitle('');
+  }
+
+  function onChangeTitle(event) {
+    setTitle(event.target.value);
+  }
+
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>List Task</h1>
+
+      <form onSubmit={handleSubmit} className={styles.newTaskForm}>
+        <input placeholder="Agrega nueva Tarea" type="text" onChange={onChangeTitle} value={title} />
+        <button>Add New Task <AiOutlinePlusCircle size={20} /></button>
+      </form>
+    </header>
+  )
+}
